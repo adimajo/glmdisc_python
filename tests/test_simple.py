f91fc1aa0ad23e6a9cf5c318a0c54812173d3eb3
adimajo <ztech1.25@gmail.com>
2020-07-01 13:47:49 +0200

    clean up and docs

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,6 +2,21 @@
 # testing in general, but rather to support the `find_packages` example in
 # setup.py that excludes installing the "tests" package
 
+from unittest import TestCase
 
-def test_success():
-    assert True
+import glmdisc.glmdisc as glmdisc
+
+class Testglmdisc(TestCase):
+
+    x, y = glmdisc.generate_data(800, 2, 1)
+    # Paramètres
+    test = False
+    validation = False
+    criterion = 'bic'
+    iter = 200
+    m_depart = 10
+
+    def test_is_string(self, x, y, test, validation, criterion, iter, m_depart):
+        essai = glmdisc(x, y, iter, m_depart)
+
+        #self.assertTrue(isinstance(s, basestring))
