578234070b9ce45e3afb05df0f59ae7e65ae5b74
adimajo <ztech1.25@gmail.com>
2020-07-10 17:23:30 +0200

    increasing all samples + iterations to avoid 'notfittederror'

--- tests/test_discrete_data.py
+++ tests/test_discrete_data.py
@@ -7,18 +7,18 @@ import glmdisc
 
 
 def test_discrete_data_cont():
-    n = 100
+    n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
-    model = glmdisc.Glmdisc(validation=False, test=False, iter=20)
+    model = glmdisc.Glmdisc(validation=False, test=False, iter=50)
     model.fit(predictors_cont=x, predictors_qual=None, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 100
+    assert result.shape[0] == 500
 
 
 def test_discrete_data_qual():
-    n = 100
+    n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
     cuts = ([0, 0.333, 0.666, 1])
@@ -27,54 +27,54 @@ def test_discrete_data_qual():
     for i in range(d):
         xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
 
-    model = glmdisc.Glmdisc(validation=False, test=False, iter=20)
+    model = glmdisc.Glmdisc(validation=False, test=False, iter=50)
     model.fit(predictors_cont=None, predictors_qual=xd, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 100
+    assert result.shape[0] == 500
 
 
 def test_discrete_data_val_cont():
+    n = 1000
+    d = 2
+    x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
+    model = glmdisc.Glmdisc(validation=True, test=False, iter=80)
+    model.fit(predictors_cont=x, predictors_qual=None, labels=y)
+    result = model.discrete_data()
+    assert isinstance(result, scipy.sparse.csr.csr_matrix)
+    assert result.shape[0] == 400
+
+
+def test_discrete_data_val_qual():
     n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
-    model = glmdisc.Glmdisc(validation=True, test=False, iter=60)
+    cuts = ([0, 0.333, 0.666, 1])
+    xd = np.ndarray.copy(x)
+
+    for i in range(d):
+        xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
+
+    model = glmdisc.Glmdisc(validation=True, test=False, iter=50)
+    model.fit(predictors_cont=None, predictors_qual=xd, labels=y)
+    result = model.discrete_data()
+    assert isinstance(result, scipy.sparse.csr.csr_matrix)
+    assert result.shape[0] == 200
+
+
+def test_discrete_data_test_cont():
+    n = 500
+    d = 2
+    x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
+    model = glmdisc.Glmdisc(validation=False, test=True, iter=50)
     model.fit(predictors_cont=x, predictors_qual=None, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
     assert result.shape[0] == 200
 
 
-def test_discrete_data_val_qual():
-    n = 100
-    d = 2
-    x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
-    cuts = ([0, 0.333, 0.666, 1])
-    xd = np.ndarray.copy(x)
-
-    for i in range(d):
-        xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
-
-    model = glmdisc.Glmdisc(validation=True, test=False, iter=20)
-    model.fit(predictors_cont=None, predictors_qual=xd, labels=y)
-    result = model.discrete_data()
-    assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 40
-
-
-def test_discrete_data_test_cont():
-    n = 100
-    d = 2
-    x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
-    model = glmdisc.Glmdisc(validation=False, test=True, iter=20)
-    model.fit(predictors_cont=x, predictors_qual=None, labels=y)
-    result = model.discrete_data()
-    assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 40
-
-
 def test_discrete_data_test_qual():
-    n = 100
+    n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
     cuts = ([0, 0.333, 0.666, 1])
@@ -83,15 +83,15 @@ def test_discrete_data_test_qual():
     for i in range(d):
         xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
 
-    model = glmdisc.Glmdisc(validation=False, test=True, iter=20)
+    model = glmdisc.Glmdisc(validation=False, test=True, iter=50)
     model.fit(predictors_cont=None, predictors_qual=xd, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 40
+    assert result.shape[0] == 200
 
 
 def test_discrete_data_both():
-    n = 100
+    n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
     cuts = ([0, 0.333, 0.666, 1])
@@ -100,15 +100,15 @@ def test_discrete_data_both():
     for i in range(d):
         xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
 
-    model = glmdisc.Glmdisc(validation=False, test=False, iter=20)
+    model = glmdisc.Glmdisc(validation=False, test=False, iter=50)
     model.fit(predictors_cont=x, predictors_qual=xd, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 100
+    assert result.shape[0] == 500
 
 
 def test_discrete_data_val_both():
-    n = 300
+    n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
     cuts = ([0, 0.333, 0.666, 1])
@@ -117,15 +117,15 @@ def test_discrete_data_val_both():
     for i in range(d):
         xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
 
-    model = glmdisc.Glmdisc(validation=True, test=False, iter=20)
+    model = glmdisc.Glmdisc(validation=True, test=False, iter=50)
     model.fit(predictors_cont=x, predictors_qual=xd, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 120
+    assert result.shape[0] == 200
 
 
 def test_discrete_data_test_both():
-    n = 100
+    n = 500
     d = 2
     x, y, theta = glmdisc.Glmdisc.generate_data(n, d)
     cuts = ([0, 0.333, 0.666, 1])
@@ -134,8 +134,8 @@ def test_discrete_data_test_both():
     for i in range(d):
         xd[:, i] = pd.cut(x[:, i], bins=cuts, labels=[0, 1, 2])
 
-    model = glmdisc.Glmdisc(validation=False, test=True, iter=20)
+    model = glmdisc.Glmdisc(validation=False, test=True, iter=50)
     model.fit(predictors_cont=x, predictors_qual=None, labels=y)
     result = model.discrete_data()
     assert isinstance(result, scipy.sparse.csr.csr_matrix)
-    assert result.shape[0] == 40
+    assert result.shape[0] == 200
